8b098e0a1ef751ded7b11885891f748b232c646b
DESKTOP-OQI8PT1\golde <goldentimebbqlove@gmail.com>
2017-05-15 14:57:35 +0900

    弾の反射調整　ファイルの整理

---
+++ Assets/Scripts/BallShooter.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShooter : MonoBehaviour
+{
+    public GameObject redBall;     //ドロップボールのプレハブ
+    public GameObject blueBall;     //ドロップボールのプレハブ
+    public GameObject yellowBall;     //ドロップボールのプレハブ
+    public GameObject greenBall;     //ドロップボールのプレハブ
+    public float timeOut;       //目標時間
+    public float apearPosX;    //ドロップボールの出現位置限界X
+    public float apearPosY;    //ドロップボールの出現位置限界Y
+    public float ballSpeed;     //ドロップボールの速さ
+    private float timeElapsed;  //時間経過
+    private Vector3 center;     //とりあえず中心に向かう
+
+
+    void Start ()
+    {
+
+	}
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;  //時間更新
+
+        //if目標時間に到達したか否か
+        if (timeElapsed >= timeOut)
+        {
+            timeElapsed = 0.0f;         //経過時間初期化
+
+            //発射位置ランダム設定ここから
+            float x = 0.0f, y = 0.0f;
+
+            x = Random.Range(-3.0f, 3.0f);  //X座標を軸にランダム設定
+
+            if (x > -apearPosX && x < apearPosX)
+            {
+                y = Random.Range(-7.0f, 7.0f);
+                while (y > -apearPosY && y < apearPosY)  //指定範囲外の時ループする
+                {
+                    y = Random.Range(-7.0f, 7.0f);
+                }
+            }
+
+            else
+            {
+                y = Random.Range(-7.0f, 7.0f);
+                while (y < -apearPosY || y > apearPosY)  //指定範囲外の時ループする
+                {
+                    y = Random.Range(-7.0f, 7.0f);
+                }
+            }
+
+            Vector3 pos = transform.position;
+            pos.x = x;
+            pos.y = y;
+            transform.position = pos;
+            //発射位置ランダム設定ここまで
+
+            int color = Random.Range(0, 4);
+
+            Vector3 force = center - transform.position;            //発射ベクトル設定
+            Vector3.Normalize(force);                               //ベクトルの正規化
+            force *= ballSpeed;                                     //ドロップボールの速さ調整
+
+            switch (color)
+            {
+                case 0:
+                    GameObject redBalls = Instantiate(redBall) as GameObject;     //ドロップボールの複製
+                    redBalls.transform.position = transform.position;          //ドロップボール発射位置設定
+                    redBalls.GetComponent<Rigidbody>().AddForce(force);        //ドロップボール発射
+                    break;
+
+                case 1:
+                    GameObject blueBalls = Instantiate(blueBall) as GameObject;     //ドロップボールの複製
+                    blueBalls.transform.position = transform.position;          //ドロップボール発射位置設定
+                    blueBalls.GetComponent<Rigidbody>().AddForce(force);        //ドロップボール発射
+                    break;
+
+                case 2:
+                    GameObject yellowBalls = Instantiate(yellowBall) as GameObject;     //ドロップボールの複製
+                    yellowBalls.transform.position = transform.position;          //ドロップボール発射位置設定
+                    yellowBalls.GetComponent<Rigidbody>().AddForce(force);        //ドロップボール発射
+                    break;
+
+                case 3:
+                    GameObject greenBalls = Instantiate(greenBall) as GameObject;     //ドロップボールの複製
+                    greenBalls.transform.position = transform.position;          //ドロップボール発射位置設定
+                    greenBalls.GetComponent<Rigidbody>().AddForce(force);        //ドロップボール発射
+                    break;
+            }
+        }
+    }
+}
