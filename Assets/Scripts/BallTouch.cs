55ad73c8b0363b6527bfcba470c6ed3ad947ad2d
nakayamakazuma <gumi.green.m793s@gmail.com>
2017-05-21 21:03:26 +0900

    タッチしたとこを云々(途中)

---
+++ Assets/Scripts/BallTouch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace GodTouches
+{
+	public class BallTouch : Ball {
+		bool touch = false;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if( touch )
+			{
+				
+			}
+		}
+
+		public override void AfterTouch()
+		{
+			touch = true;
+			Vector3 vec = GodTouch.GetPosition();
+			vec = GodTouch.GetPosition();
+			//vec.z = 30.0f;
+			vec = Camera.main.ScreenToWorldPoint( vec );
+			Debug.Log( "x:" + vec.x + "y:" + vec.y + "z:" + vec.z );
+		}
+
+	}
+}
---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodTouches
+{
+	public class Touch : MonoBehaviour {
+		const float distance = 100.0f;
+		Vector3 touchPos;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			touchPos = GodTouch.GetPosition();
+			if( GodTouch.GetPhase() == GodPhase.Began )
+			{
+				Debug.Log( "x:" + touchPos.x + "y:" + touchPos.y + "z:" + touchPos.z );
+				Ray ray = Camera.main.ScreenPointToRay( touchPos );
+				RaycastHit hit = new RaycastHit();
+
+				if( Physics.Raycast( ray, out hit, distance ) )
+				{
+					GameObject touchBall = hit.collider.gameObject;
+					Ball ballComponent = touchBall.GetComponent<Ball>();
+					if( ballComponent != null )
+					{
+						ballComponent.AfterTouch();
+					}
+				}
+			}			
+		}
+	}
+}
