9d900dd4bfe69a4f8e83b2959a789c4d55f4f100
nakayamakazuma <gumi.green.m793s@gmail.com>
2017-06-02 17:07:23 +0900

    タップした方向に弾が飛ぶやつ

---
+++ Assets/Scripts/CoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodTouches{
+	public class CoreManager : MonoBehaviour {
+
+		public GameObject prefab;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if(GodTouch.GetPhase() == GodPhase.Began)
+			{
+				GameObject bullets = Instantiate(prefab) as GameObject;
+			}
+		}
+	}
+}
