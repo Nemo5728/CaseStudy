cf4b6512319aaa2558d2485dd7f96f2d3ceffccc
KadokawaRyota <ryota.creategame@gmail.com>
2017-06-12 12:18:12 +0900

    ロゴ、テクスチャ　シーンを増本が追加した。のを代理うｐ

---
+++ Assets/Scripts/SceneScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneScript : MonoBehaviour {
+
+	public void LoadSceneGame(){
+		Application.LoadLevel ("TeseScene");
+	}
+}
